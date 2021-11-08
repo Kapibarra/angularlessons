5cfbc8f70601397ce109ce84a316a570f58df50f
Nikita <69010558+Kapibarra@users.noreply.github.com>
2021-11-08 14:35:46 +0300

    bindings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angularlessons';
+
+  title = 'Dynamic title'
+  number = 42
+  arr = [1, 2, 3]
+  obj = { a: 1, b: {c: 2} }
+  img = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTsm-dQHFmWp9Xw1e-4BfLDr67vBq5cil6OytRJExumqHUzTHVZ'
+
+  constructor() {
+    setTimeout(() => {
+      console.log('Timeout is over')
+      this.img = 'https://angular.io/assets/images/logos/angular/angular.png'
+    }, 5000)
+  }
+
+
+
 }
--- src/app/post2/post2.component.spec.ts
+++ src/app/post2/post2.component.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { Post2Component } from './post2.component';
 
@@ -6,12 +6,12 @@ describe('Post2Component', () => {
   let component: Post2Component;
   let fixture: ComponentFixture<Post2Component>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
       declarations: [ Post2Component ]
     })
     .compileComponents();
-  });
+  }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(Post2Component);
