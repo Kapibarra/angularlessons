f80fe6227c836d87ce89e2abf4d9455fd3569dcf
Nikita <69010558+Kapibarra@users.noreply.github.com>
2021-11-09 17:09:11 +0300

    ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+arr = [1,1,2,3,5,8,13]
 
-Toggle:any = false;
+objects = [{
+  title: 'Post 1', author:'Nikita', comments: [{
+    name:'max', text:'lorem 1'
+  },
+  {
+    name:'max', text:'lorem 1'
+  },
+  {
+    name:'max', text:'lorem 1'
+  },]
+},
+{
+  title: 'Post 2', author:'Nikita2', comments: [
+  {name:'max', text:'lorem 2'},
+  {name:'max', text:'lorem 2'},
+  {name:'max', text:'lorem 2'},]
+}
+]
 
 }
