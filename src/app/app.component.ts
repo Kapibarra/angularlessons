558385a51e19ef87e779dac751f90393d8bfb040
Nikita <69010558+Kapibarra@users.noreply.github.com>
2021-11-09 18:56:10 +0300

    parameters 02

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 
+export interface Post {
+  title: string
+  text: string
+  id?: number
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,6 +13,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-  now : Date = new Date()
-  
+posts: Post[] = [
+  {title: 'Angular JS', text: 'Post text', id: 1},
+  {title: 'Angular JS 2', text: 'Post text 2', id: 2},
+];
+
 }
