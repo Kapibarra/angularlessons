f27fab02e3d0d6e775e8ea28cb04a9eaf2e57ce0
Nikita <69010558+Kapibarra@users.noreply.github.com>
2022-01-10 13:38:11 +0300

    604 service scopes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import {NgModule} from '@angular/core'
 
 import {AppComponent} from './app.component'
 import {FormsModule} from '@angular/forms';
-import {AppCounterService} from './services/app-counter.service'
+import {AppCounterService} from './services/app-counter.service';
+import { CounterComponent } from './counter/counter.component'
 
 @NgModule({
   declarations: [
     AppComponent,
+    CounterComponent,
   ],
   imports: [
     BrowserModule,
