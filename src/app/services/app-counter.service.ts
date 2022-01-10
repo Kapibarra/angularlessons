150d30cd82858821a1d082d1fa9081f771f8fb48
Nikita <69010558+Kapibarra@users.noreply.github.com>
2022-01-10 13:41:40 +0300

    605 service connect

--- src/app/services/app-counter.service.ts
+++ src/app/services/app-counter.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from "@angular/core"
+import { LogService } from "./log.service"
 
 @Injectable({
   providedIn: 'root'
@@ -6,12 +7,16 @@ import { Injectable } from "@angular/core"
 
 export class AppCounterService {
   counter = 0
+constructor(private logService: LogService) {
 
+}
   increase() {
     this.counter++
+    this.logService.log('increase counter...')
   }
 
   decrease() {
     this.counter--
+    this.logService.log('decrease counter...')
   }
 }
