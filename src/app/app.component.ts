3264c3a2edb5c6dc1d98bb7eccf0e997432bb2d0
Nikita <69010558+Kapibarra@users.noreply.github.com>
2022-01-17 16:27:35 +0300

    802 json http

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
+import { HttpClient } from '@angular/common/http';
 import {Component, OnInit} from '@angular/core'
+
+export interface Todo {
+  completed: boolean;
+  title: string;
+  id?: number
+}
 
 @Component({
   selector: 'app-root',
@@ -7,8 +14,17 @@ import {Component, OnInit} from '@angular/core'
 })
 export class AppComponent implements OnInit {
 
-  ngOnInit() {
+constructor(private http: HttpClient) {}
 
+todos: Todo[] = [];
+
+  ngOnInit() {
+this.http.get<Todo[]>('https://jsonplaceholder.typicode.com/todos?_limit=5')
+.subscribe(todos => {
+  console.log(todos);
+  this.todos = todos;
+  
+})
   }
 }
 
