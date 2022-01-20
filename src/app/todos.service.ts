7645c21ef05ff1ebe2c832dfbf06096c10ce5545
Nikita <69010558+Kapibarra@users.noreply.github.com>
2022-01-20 16:17:12 +0300

    811 observe

--- src/app/todos.service.ts
+++ src/app/todos.service.ts
@@ -1,7 +1,7 @@
 import {Injectable} from '@angular/core'
-import {HttpClient, HttpHeaders, HttpParams} from '@angular/common/http'
+import {HttpClient, HttpEventType, HttpHeaders, HttpParams} from '@angular/common/http'
 import {Observable, throwError} from 'rxjs'
-import {catchError, delay} from 'rxjs/operators'
+import {catchError, delay, map, tap} from 'rxjs/operators'
 
 export interface Todo {
   completed: boolean
@@ -29,10 +29,14 @@ export class TodosService {
     params = params.append('custom', 'anything')
 
     return this.http.get<Todo[]>('https://jsonplaceholder.typicode.com/todos', {
-      // params: new HttpParams().set('_limit', '3')
-      params
+      params,
+      observe: 'response'
     })
       .pipe(
+        map(response => {
+          // console.log('Response', response)
+          return response.body
+        }),
         delay(500),
         catchError(error => {
           console.log('Error: ', error.message)
@@ -41,13 +45,27 @@ export class TodosService {
       )
   }
 
-  removeTodo(id: number): Observable<void> {
-    return this.http.delete<void>(`https://jsonplaceholder.typicode.com/todos/${id}`)
+  removeTodo(id: number): Observable<any> {
+    return this.http.delete<void>(`https://jsonplaceholder.typicode.com/todos/${id}`, {
+      observe: 'events'
+    }).pipe(
+      tap(event => {
+        if (event.type === HttpEventType.Sent) {
+          console.log('Sent', event)
+        }
+
+        if (event.type === HttpEventType.Response) {
+          console.log('Response', event)
+        }
+      })
+    )
   }
 
-  completeTodo(id: number): Observable<Todo> {
+  completeTodo(id: number): Observable<any> {
     return this.http.put<Todo>(`https://jsonplaceholder.typicode.com/todos/${id}`, {
       completed: true
+    }, {
+      responseType: 'json'
     })
   }
 }
