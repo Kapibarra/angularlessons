491d04746dd6d6457bd8572c4b78d7d53dc5543d
Nikita <69010558+Kapibarra@users.noreply.github.com>
2022-01-20 16:15:54 +0300

    810 query params

--- src/app/todos.service.ts
+++ src/app/todos.service.ts
@@ -1,7 +1,7 @@
 import {Injectable} from '@angular/core'
-import {HttpClient, HttpHeaders} from '@angular/common/http'
-import {Observable} from 'rxjs'
-import {delay} from 'rxjs/operators'
+import {HttpClient, HttpHeaders, HttpParams} from '@angular/common/http'
+import {Observable, throwError} from 'rxjs'
+import {catchError, delay} from 'rxjs/operators'
 
 export interface Todo {
   completed: boolean
@@ -14,16 +14,31 @@ export class TodosService {
   constructor(private http: HttpClient) {}
 
   addTodo(todo: Todo): Observable<Todo> {
+    const headers = new HttpHeaders({
+      'MyCustomHeader': Math.random().toString(),
+    })
+
     return this.http.post<Todo>('https://jsonplaceholder.typicode.com/todos', todo, {
-      headers: new HttpHeaders( {
-        'My CustomHeader' : Math.random().toString()
-      })
+      headers
     })
   }
 
   fetchTodos(): Observable<Todo[]> {
-    return this.http.get<Todo[]>('https://jsonplaceholder.typicode.com/todos?_limit=2')
-      .pipe(delay(500))
+    let params = new HttpParams()
+    params = params.append('_limit', '4')
+    params = params.append('custom', 'anything')
+
+    return this.http.get<Todo[]>('https://jsonplaceholder.typicode.com/todos', {
+      // params: new HttpParams().set('_limit', '3')
+      params
+    })
+      .pipe(
+        delay(500),
+        catchError(error => {
+          console.log('Error: ', error.message)
+          return throwError(error)
+        })
+      )
   }
 
   removeTodo(id: number): Observable<void> {
@@ -31,8 +46,8 @@ export class TodosService {
   }
 
   completeTodo(id: number): Observable<Todo> {
-  return this.http.put<Todo>(`https://jsonplaceholder.typicode.com/todos/${id}`, {
-  completed: true
-  })
+    return this.http.put<Todo>(`https://jsonplaceholder.typicode.com/todos/${id}`, {
+      completed: true
+    })
   }
 }
