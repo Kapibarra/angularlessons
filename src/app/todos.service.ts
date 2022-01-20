bb1bf7f4b5acfb6787dda4cb15467c26f099dcc4
Nikita <69010558+Kapibarra@users.noreply.github.com>
2022-01-20 16:12:10 +0300

    809 http headers

--- src/app/todos.service.ts
+++ src/app/todos.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core'
-import {HttpClient} from '@angular/common/http'
+import {HttpClient, HttpHeaders} from '@angular/common/http'
 import {Observable} from 'rxjs'
 import {delay} from 'rxjs/operators'
 
@@ -14,7 +14,11 @@ export class TodosService {
   constructor(private http: HttpClient) {}
 
   addTodo(todo: Todo): Observable<Todo> {
-    return this.http.post<Todo>('https://jsonplaceholder.typicode.com/todos', todo)
+    return this.http.post<Todo>('https://jsonplaceholder.typicode.com/todos', todo, {
+      headers: new HttpHeaders( {
+        'My CustomHeader' : Math.random().toString()
+      })
+    })
   }
 
   fetchTodos(): Observable<Todo[]> {
