13c4f8ce21e413da350a966dacd9f3c9a10b8e08
Nikita <69010558+Kapibarra@users.noreply.github.com>
2022-01-24 11:44:52 +0300

    1001 animation init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,10 @@
-import {Component} from '@angular/core'
-import { AuthService } from './auth.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  constructor(private auth: AuthService) {}
-}
 
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,16 @@
-import {BrowserModule} from '@angular/platform-browser'
-import {NgModule} from '@angular/core'
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import {AppComponent} from './app.component'
-import {FormsModule} from '@angular/forms'
-import {AboutComponent} from './about/about.component'
-import {HomeComponent} from './home/home.component'
-import {PostsComponent} from './posts/posts.component'
-import {PostComponent} from './post/post.component'
-import {AboutExtraComponent} from './about-extra/about-extra.component'
-import { AppRoutingModule } from './app-routing.module';
-import { ErrorPageComponent } from './error-page/error-page.component'
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
 @NgModule({
   declarations: [
-    AppComponent,
-    AboutComponent,
-    HomeComponent,
-    PostsComponent,
-    PostComponent,
-    AboutExtraComponent,
-    ErrorPageComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    AppRoutingModule
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
