123efccf094d6f294ba2b532ff805fe2b76963a8
Nikita <69010558+Kapibarra@users.noreply.github.com>
2022-01-24 12:54:17 +0300

    1004 advanced transitions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,55 @@
-import { animate, state, style, transition, trigger } from '@angular/animations';
-import { Component } from '@angular/core';
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from "@angular/animations";
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
   animations: [
-    trigger('box', [
-      state('start', style({
-        background: 'blue',
-      })),
-      state('end', style({
-        background: 'red',
-        transform: 'scale(1.2)',
-      })),
-      transition('start => end', animate(450)),
-      transition('end => start', animate('950ms ease-in'))
-    ])
-  ]
+    trigger("box", [
+      state(
+        "start",
+        style({
+          background: "blue",
+        })
+      ),
+      state(
+        "end",
+        style({
+          background: "red",
+          transform: "scale(1.2)",
+        })
+      ),
+      state(
+        "special",
+        style({
+          background: "orange",
+          transform: "scale(0.5)",
+          borderRadius: "50%",
+        })
+      ),
+      transition("start => end", animate(450)),
+      transition("end => start", animate("950ms ease-in")),
+      // transition("special => *", animate(500)),
+      transition("* <=> special", [
+        style({ background: "green", transform: "scale(1.5)" }),
+        animate(500),
+        style({ background: "pink" }),
+        animate(500),
+      ]),
+    ]),
+  ],
 })
 export class AppComponent {
-boxState = 'start';
+  boxState = "start";
 
-animate() {
-  this.boxState = this.boxState === 'end' ? 'start' : 'end';
-}
+  animate() {
+    this.boxState = this.boxState === "end" ? "start" : "end";
+  }
 }
